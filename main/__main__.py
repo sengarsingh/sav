49cb3a0c59f8793cf464adda645edd514d615a17
Apnijanta <159753576+Apnijanta@users.noreply.github.com>
2024-06-13 08:41:30 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ botStartTime = time.time()
 
 
 print("Successfully deployed!")
-print("By Mr. Invisible • Mr_Invisible_bots")
+print("By apnijanta • apni_janta")
 
 if __name__ == "__main__":
     from . import bot
