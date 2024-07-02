4cd392453c1c306fb16dd12ac15e19e6616ffb59
sengarsingh <149374164+sengarsingh@users.noreply.github.com>
2024-07-02 15:40:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+#Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "fill your")
+
+#Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "Fill your"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "Fill your")
+SESSION = os.environ.get("SESSION", "Fill your")
+FORCESUB = os.environ.get("FORCESUB", "Fill your")
+AUTH = os.environ.get("AUTH", "Fill your")
