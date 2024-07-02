963c0bb60ed3237b8359249e891d88d5759e8be3
sengarsingh <149374164+sengarsingh@users.noreply.github.com>
2024-07-02 15:46:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "fill your")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6363235576:AAGZ39hHsKJrPS9iVEotYDf4wcxQ5qszYck")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "Fill your"))
+API_ID = int(os.environ.get("API_ID", "29939868"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "Fill your")
-SESSION = os.environ.get("SESSION", "Fill your")
-FORCESUB = os.environ.get("FORCESUB", "Fill your")
-AUTH = os.environ.get("AUTH", "Fill your")
+API_HASH = os.environ.get("API_HASH", "1ee10fce7cb72aaf315b97f4b33decc0")
+SESSION = os.environ.get("SESSION", "BQBghPyfId8Tdo2ZsYTHLCof0f09hBTBUJ-1D_0W3R3I1YukTk1Wozn0bETNmlmFKhjE7fIvD9qlVnKXtGY_WWKRY6TSPxULkXKCPXeBU89VNIviAcYummHmIimocxJbFyH29vIkTTiJC2RIieYa3jOnDxYP7OqNNNvrdbh5Dsl_9Lyl3wl6Bguh8TUrBp9GQeyHFdIqGQclQechY8MwirfZLhORa8FF4tjJTnsSotMajytNJI_93uoqr_5UFuV-u0uf94eQh1hQWeHvPwY_B5LR1916F_VGKDTpbkW-aJNqMnczRGaB4xJlHsgwPFb-pzulFS_9pLGLBVXporhdZpUWAAAAAT95R7IA")
+FORCESUB = os.environ.get("FORCESUB", "aravsave")
+AUTH = os.environ.get("AUTH", "5359880114            ")
